893cb6ddc7baf8f57bc308ff033d7b782a9bb569
Emanuele B <buonaccorsiemanuele@gmail.com>
2024-02-22 11:14:56 +0100

    stats page with automatic refresh

--- StatsView.swift
+++ StatsView.swift
@@ -1,20 +1,28 @@
-//
-//  StatsView.swift
-//  primo-playground
-//
-//  Created by Emanuele Buonaccorsi on 09/02/24.
-//
-
 import SwiftUI
 
 struct StatsView: View {
+    
+    @State private var refreshCount = 0
+    
+    
     var body: some View {
         ZStack {
             Color.appBackground
                 .edgesIgnoringSafeArea(.all)
             
-            Text("Hello, Stats!")
+            VStack{
+                Text("Hello, Stats!")
+                VStack {
+                    Text("You won \(UserStatistics.totalTimesWon) times so far.")
+                    Text("Consumed an average of \(UserStatistics.averageLitersConsumed) L.")
+                    Text("Saved \(UserStatistics.totalLitersSaved) L.")
+                }
+            }.id(refreshCount)
             
+            
+        }
+        .onAppear {
+            refreshCount += 1
         }
     }
 }
