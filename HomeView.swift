33fd3d97d1ea13dff626cc28a97db58a1e59d40f
Emanuele B <buonaccorsiemanuele@gmail.com>
2024-02-24 10:51:03 +0100

    refactored by moving shower logic to its own class

--- HomeView.swift
+++ HomeView.swift
@@ -1,46 +1,20 @@
 import SwiftUI
 import Combine
-import AVFoundation
+
 
 struct HomeView: View {
 
-    @State var showerData = ShowerData();
-
-    
-    @State private var isStarted: Bool = false;
-    
-    @State private var timeSoFar : String = "00:00";
-    @State private var timeLeft : String = "00:00";
-    
-    @State private var isPastMaxTime : Bool = false;
     
     @State private var waterMessagesManager = WaterMessagesManager();
 
-    
     @State private var showEndAlert : Bool = false;
     @State private var alertMessage : String = "";
     
-    @State private var audioManager = AudioManager();
-    
-
-    
-    
-    // singleton instance
+    // singleton instances
     @ObservedObject var mySettings = ShowerSettingsManager.shared;
     @ObservedObject var myUserStats = UserStats.shared;
+    @ObservedObject var showerData = ShowerData.shared;
     
-    
-    
-    // pause variables ---------
-    
-    // timestamp of the last time the "pause" button was pressed
-    // if the timer is not in pause right now, it must be nil
-    @State private var currentPauseStartTimestamp : Date? = nil;
-    
-    // time passed during various pauses, to be subtracted when calculating the time so far, and updated when ending a pause or the shower
-    @State private var totalPausedTime : Int = 0;
-    
-
 
     
     private var timerPublisher = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -74,7 +48,7 @@ struct HomeView: View {
                 
                 
                 // Show "End Shower" button only if the timer is active
-                if isStarted {
+                if showerData.isRunning {
                     
                     
                     Button("End Shower") {
@@ -83,11 +57,11 @@ struct HomeView: View {
                     .padding()
                     
                     
-                    Button(action: handlePause) {
-                                    Text(isPaused() ? "Resume" : "Pause")
+                    Button(action: showerData.togglePause) {
+                        Text(showerData.isPaused ? "Resume" : "Pause")
                                         .font(.title)
                                         .padding()
-                                        .background(isPaused() ? Color.green : Color.red)
+                                        .background(showerData.isPaused ? Color.green : Color.red)
                                         .foregroundColor(.white)
                                         .cornerRadius(10)
                     }
@@ -96,7 +70,7 @@ struct HomeView: View {
 
                     
                     
-                    if isPastMaxTime {
+                    if showerData.isPastMaxTime {
                         // TODO: animazioni e suoni
                         Text("Time to end your shower!")
                             .font(.largeTitle)
@@ -112,8 +86,7 @@ struct HomeView: View {
             
                     } else {
                         HStack {
-                            // Text("\(timeSoFar)")
-                            Text("\(timeLeft)")
+                            Text("\(showerData.timeLeft)")
                         }
                         .padding()
                     }
@@ -126,7 +99,7 @@ struct HomeView: View {
                 } else {
                     // Show "Start Shower" button only if the timer hasn't started
                     Button("Start Shower") {
-                        startTimer()
+                        showerData.start()
                     }
                     .padding()
                 }
@@ -141,7 +114,7 @@ struct HomeView: View {
         .onReceive(timerPublisher) { _ in
             // when I recieve an event from the timer
             // I update the timer values (every 1 second)
-            updateTimerAndData()
+            showerData.update()
         }
         .alert(isPresented: $showEndAlert) {
             Alert(title: Text("Shower Result"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
@@ -158,27 +131,9 @@ struct HomeView: View {
     
     
     
-    func isPaused() -> Bool {
-        return currentPauseStartTimestamp != nil;
-    }
-
-    func startTimer() {
-        debugPrint("Starting timer!");
-        
-        showerData.startTime = Date();
-        updateTimerAndData();
-        isStarted = true;
-        updateTimerAndData();
-
-    }
-    
     func endShower() {
-        debugPrint("Ending shower!");
-        audioManager.stopSound()
-        updateTimerAndData(); // update data one last time
-        isStarted = false;
-        showerData.endTime = Date();
-        showerData.won = showerData.showerDuration <= (mySettings.maxShowerTime + mySettings.gracePeriod);
+        showerData.update()
+        showerData.end();
         
         // TODO: save the shower data
         myUserStats.saveNewShower(
@@ -187,85 +142,16 @@ struct HomeView: View {
             litersSaved: 1 // TODO: calculate extimation liters saved
         );
 
-        
-        
-        
-        // TODO: celebrate won or loss in a nice way ------
-        
+        // TODO: celebrate won or loss in a nice way
         // Show alert to celebrate win or loss
         alertMessage = showerData.won ? "Congratulations! You saved water!" : "Oops! You exceeded the time. :(\n";
-        
-        // alertMessage += " You won \(myUserStats.totalTimesWon) times so far, consumed an avaerage of \(myUserStats.averageLitersConsumed) L, and saved \(myUserStats.totalLitersSaved) L.";
-        
         showEndAlert = true;
         //TODO: confetti animation
         
-        
-        // reset showerData and pause tracker ------
-        showerData = ShowerData(); // resets
-        audioManager = AudioManager();
-        currentPauseStartTimestamp = nil;
-        totalPausedTime = 0;
-        isPastMaxTime = false;
-        
+        showerData.reset()
+        showerData.update()
     }
-    
-    func handlePause() {
-        audioManager.stopSound()
-        if let t = currentPauseStartTimestamp {
-            totalPausedTime += Int(Date().timeIntervalSince(t));
-            currentPauseStartTimestamp = nil;
-        } else {
-            currentPauseStartTimestamp = Date();
-        }
-    }
-    
-    
-    
-    
-    
-    
-    /// Updates the variables so that they show the time passed so far.
-    ///
-    /// this function is ran (possibly) once every second
-    func updateTimerAndData() {
-        guard isStarted else { return }
         
-        guard let startTime = showerData.startTime else {
-            print("Error: startTime is nil, but isStarted is true!")
-            return;
-        }
-        
-        // in this case, we are in a pause. we do not update the view.
-        guard (self.currentPauseStartTimestamp == nil) else { return }
-
-        
-        
-        showerData.showerDuration = Int(Date().timeIntervalSince(startTime)) - totalPausedTime;
-        
-        
-        showerData.litersConsumed = Int(Double(mySettings.litersPerMinute) * Double(showerData.showerDuration) / 60)
-        
-        // time so far to be shown in the view
-        let minutesSoFar = showerData.showerDuration / 60
-        let secondsSoFar = showerData.showerDuration % 60
-        timeSoFar = String(format: "%02d:%02d", minutesSoFar, secondsSoFar)
-        
-        // time left to be shown
-        let remainingTime : Int = mySettings.maxShowerTime - showerData.showerDuration;
-        let minutesLeft = Int(remainingTime) / 60
-        let secondsLeft = Int(remainingTime) % 60
-        timeLeft = String(format: "%02d:%02d", minutesLeft, secondsLeft)
-    
-    
-        // Check if the maximum shower time is reached
-        if showerData.showerDuration >= mySettings.maxShowerTime {
-            isPastMaxTime = true;
-            // TODO: decide when to notify with sound: in anticipo o esattamente?
-            audioManager.playSound()
-        }
-    }
-    
     
     
 
@@ -279,47 +165,6 @@ struct HomeView: View {
 
 
 
-
-class AudioManager {
-    
-    private var player: AVAudioPlayer?
-    private var lastPlayTime: Date
-    
-    init() {
-        lastPlayTime = Date.distantPast
-    }
-
-    /// Plays the sound only if it hasn't been played recently (in the last `interval` seconds)
-    func playSound(interval: TimeInterval = 30) {
-        guard let url = Bundle.main.url(forResource: "tyla_water", withExtension: "mp3") else {
-            print("Error: unable to find file")
-            return
-        }
-
-        // Check if enough time has passed since the last play
-        guard Date().timeIntervalSince(lastPlayTime) >= interval else {
-            print("Audio played recently. Ignoring.")
-            return
-        }
-
-        do {
-            player = try AVAudioPlayer(contentsOf: url)
-            guard let player = player else {
-                print("Error: unable to create player")
-                return
-            }
-            player.play()
-            lastPlayTime = Date()
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
-    
-    
-    func stopSound(){
-        player?.stop()
-    }
-}
 
 
 
@@ -330,4 +175,3 @@ struct HomeView_Previews: PreviewProvider {
         HomeView()
     }
 }
-
--- ShowerData.swift
+++ ShowerData.swift
@@ -1,11 +1,182 @@
 import SwiftUI
+import AVFoundation
 
 class ShowerData: ObservableObject {
+    static var shared = ShowerData()
+    
+    private init() {}
+    
     @Published var startTime: Date?
     @Published var endTime: Date?
+    @Published var timeLeft: String = ""
     @Published var showerDuration: Int = 0
     @Published var litersConsumed: Int = 0
-    @Published var won: Bool = false;
+    @Published var won: Bool = false
+    @Published var isPastMaxTime: Bool = false
+    
+    /// true from start to end.
+    ///
+    /// can be true even if in pause.
+    @Published var isRunning: Bool = false;
+    @Published var isPaused: Bool = false;
+    
+    
+    private var audioManager = AudioManager();
+    
+    // pause variables ---------
+    
+    // timestamp of the last time the "pause" button was pressed
+    // if the timer is not in pause right now, it must be nil
+    private var currentPauseStartTimestamp : Date? = nil;
+    
+    // time passed during various pauses, to be subtracted when calculating the time so far, and updated when ending a pause or the shower
+    private var totalPausedTime : Int = 0;
+    
+
+    
+        
+    func start() {
+        startTime = Date()
+        isRunning = true;
+    }
+    
+    func end(){
+        debugPrint("Ending shower!");
+        isRunning = false;
+        audioManager.stopSound()
+        
+        self.update(); // update data one last time
+        
+        self.endTime = Date();
+        self.won = self.showerDuration <= (ShowerSettingsManager.shared.maxShowerTime + ShowerSettingsManager.shared.gracePeriod);
+    }
+    
+    
+    func reset() {
+        print("resetting")
+        startTime = nil
+        endTime = nil
+        timeLeft = "Not started yet"
+        showerDuration = 0
+        litersConsumed = 0
+        won = false
+        isPastMaxTime = false
+        isRunning = false
+        isPaused = false
+        currentPauseStartTimestamp = nil
+        totalPausedTime = 0
+        print(self.description)
+    }
+    
+    func togglePause(){
+        isPaused.toggle()
+        audioManager.stopSound()
+        
+        if let t = currentPauseStartTimestamp {
+            totalPausedTime += Int(Date().timeIntervalSince(t));
+            currentPauseStartTimestamp = nil;
+        } else {
+            currentPauseStartTimestamp = Date();
+        }
+    }
+    
+    
+    /// Updates the variables to display the remaining time and other shower data.
+    ///
+    /// This function is called periodically to update the displayed information.
+    func update() {
+        guard self.isRunning else { return }
+        
+        guard let startTime = self.startTime else {
+            print("Error: startTime is nil, but isStarted is true!")
+            return
+        }
+        
+        // Check if the shower is currently paused
+        guard currentPauseStartTimestamp == nil else { return }
+        
+        // Calculate shower duration
+        self.showerDuration = Int(Date().timeIntervalSince(startTime)) - totalPausedTime
+        
+        // Calculate liters consumed
+        self.litersConsumed = Int(Double(ShowerSettingsManager.shared.litersPerMinute) * Double(self.showerDuration) / 60)
+    
+        
+        // Calculate time left
+        let remainingTime: Int = ShowerSettingsManager.shared.maxShowerTime - self.showerDuration
+        let minutesLeft = Int(remainingTime) / 60
+        let secondsLeft = Int(remainingTime) % 60
+        self.timeLeft = String(format: "%02d:%02d", minutesLeft, secondsLeft)
+        
+        // Check if the maximum shower time is reached
+        if self.showerDuration >= ShowerSettingsManager.shared.maxShowerTime {
+            self.isPastMaxTime = true
+            // TODO: decide when to notify with sound: in advance or exactly at max time?
+            audioManager.playSound()
+        }
+    }
+    
+    
+
+
+        
+        var description: String {
+            return """
+            ShowerData:
+            - isRunning: \(isRunning)
+            - isPaused: \(isPaused)
+            - showerDuration: \(showerDuration)
+            - litersConsumed: \(litersConsumed)
+            - won: \(won)
+            - isPastMaxTime: \(isPastMaxTime)
+            """
+        }
+        
+     
+    
 }
 
+
+
+
+class AudioManager {
+    
+    private var player: AVAudioPlayer?
+    private var lastPlayTime: Date
+    
+    init() {
+        lastPlayTime = Date.distantPast
+    }
+
+    /// Plays the sound only if it hasn't been played recently (in the last `interval` seconds)
+    func playSound(interval: TimeInterval = 30) {
+        guard let url = Bundle.main.url(forResource: "tyla_water", withExtension: "mp3") else {
+            print("Error: unable to find file")
+            return
+        }
+
+        // Check if enough time has passed since the last play
+        guard Date().timeIntervalSince(lastPlayTime) >= interval else {
+            print("Audio played recently. Ignoring.")
+            return
+        }
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else {
+                print("Error: unable to create player")
+                return
+            }
+            player.play()
+            lastPlayTime = Date()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    func stopSound(){
+        player?.stop()
+    }
+}
 
